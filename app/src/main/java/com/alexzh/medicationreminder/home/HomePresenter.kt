7dc09f054f8280015925b4e57597eb7bdabd3be6
Alex <zhukovich.a@gmail.com>
2017-10-25 19:18:24 +0200

    [Refactoring] made repository property to private in HomePresenter

--- app/src/main/java/com/alexzh/medicationreminder/home/HomePresenter.kt
+++ app/src/main/java/com/alexzh/medicationreminder/home/HomePresenter.kt
@@ -1,6 +1,6 @@
 package com.alexzh.medicationreminder.home
 
-class HomePresenter(private val view: Home.View, val repository: Home.Repository) : Home.Presenter {
+class HomePresenter(private val view: Home.View, private val repository: Home.Repository) : Home.Presenter {
 
     override fun loadMore() {
         view.showLoader()
