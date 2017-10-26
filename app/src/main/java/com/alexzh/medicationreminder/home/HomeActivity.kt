e398cc7bb2efb860fd30c83eeeeadfa67ca84b34
Alex <zhukovich.a@gmail.com>
2017-10-26 23:18:22 +0200

    Added the `HomeActivity contains the Settings menu item` test case

--- app/src/main/java/com/alexzh/medicationreminder/home/HomeActivity.kt
+++ app/src/main/java/com/alexzh/medicationreminder/home/HomeActivity.kt
@@ -1,13 +1,22 @@
 package com.alexzh.medicationreminder.home
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
 import com.alexzh.medicationreminder.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.home_activity_menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
--- app/src/test/java/com/alexzh/medicationreminder/home/HomeActivityTest.kt
+++ app/src/test/java/com/alexzh/medicationreminder/home/HomeActivityTest.kt
@@ -2,13 +2,16 @@
 
 package com.alexzh.medicationreminder.home
 
+import android.support.v7.widget.Toolbar
 import android.view.View
 import com.alexzh.medicationreminder.R
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.Shadows.shadowOf
 
 @RunWith(RobolectricTestRunner::class)
 class HomeActivityTest {
@@ -19,5 +22,15 @@ class HomeActivityTest {
         val recyclerView = activity.findViewById<View>(R.id.recyclerView)
 
         assertEquals(recyclerView.visibility, View.VISIBLE)
+    }
+
+    @Test
+    fun `HomeActivity contains the Settings menu item`() {
+        val activity = Robolectric.setupActivity(HomeActivity::class.java)
+        val toolbar = activity.findViewById<View>(R.id.toolbar) as Toolbar
+        val shadowActivity = shadowOf(activity)
+        shadowActivity.onCreateOptionsMenu(toolbar.menu)
+
+        assertTrue(shadowActivity.optionsMenu.hasVisibleItems())
     }
 }
