2b6816acf448c170d34473ad25664140539d13fa
Alex <zhukovich.a@gmail.com>
2017-11-05 16:14:45 +0100

    [Refactoring] Cleaned up the PillDetailPresenter

--- app/src/main/java/com/alexzh/medicationreminder/pilldetail/PillDetailPresenter.kt
+++ app/src/main/java/com/alexzh/medicationreminder/pilldetail/PillDetailPresenter.kt
@@ -1,14 +1,21 @@
 package com.alexzh.medicationreminder.pilldetail
 
 import com.alexzh.medicationreminder.data.PillsRepository
+import com.alexzh.medicationreminder.data.model.Pill
 
 class PillDetailPresenter(private val repository: PillsRepository,
                           private val view: PillDetail.View) : PillDetail.Presenter {
 
     override fun loadPillInfo(pillId: Long) {
         repository.getPillById(pillId)
-                .subscribe(
-                        { view.showPillInfo(it) },
-                        { view.showErrorMessage() })
+                .subscribe(this::handleSuccess, this::handleError)
+    }
+
+    private fun handleSuccess(pill: Pill) {
+        view.showPillInfo(pill)
+    }
+
+    private fun handleError(t: Throwable) {
+        view.showErrorMessage()
     }
 }
