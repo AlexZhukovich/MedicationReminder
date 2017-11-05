352e4de2541822d3cde5bae446fa0142fb696639
Alex <zhukovich.a@gmail.com>
2017-11-05 12:17:03 +0100

    [Refactoring] Removed duplicated test

--- app/src/test/java/com/alexzh/medicationreminder/pilldetail/PillDetailPresenterTest.kt
+++ app/src/test/java/com/alexzh/medicationreminder/pilldetail/PillDetailPresenterTest.kt
@@ -3,7 +3,6 @@
 package com.alexzh.medicationreminder.pilldetail
 
 import com.alexzh.medicationreminder.data.PillsRepository
-import com.nhaarman.mockito_kotlin.any
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.verify
 import org.junit.Test
@@ -16,13 +15,6 @@ class PillDetailPresenterTest {
 
     private val repository = mock<PillsRepository>()
     private val presenter = PillDetailPresenter(repository)
-
-    @Test
-    fun `Call repository during loading data`() {
-        presenter.loadPillInfo(PILL_ID)
-
-        verify(repository).getPillById(any())
-    }
 
     @Test
     fun `Call repository during loading data with pill ID`() {
