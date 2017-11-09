821ade5f019093685f051c2009a014d3609faaab
Alex <zhukovich.a@gmail.com>
2017-11-09 22:43:35 +0100

    [Fix] Added initialization of repository for PillDetailActivity

--- app/src/main/java/com/alexzh/medicationreminder/MedicationReminderApp.kt
+++ app/src/main/java/com/alexzh/medicationreminder/MedicationReminderApp.kt
@@ -3,6 +3,7 @@ package com.alexzh.medicationreminder
 import android.app.Application
 import com.alexzh.medicationreminder.data.local.LocalPillsPillsRepository
 import com.alexzh.medicationreminder.home.HomeActivity
+import com.alexzh.medicationreminder.pilldetail.PillDetailActivity
 import timber.log.Timber
 
 class MedicationReminderApp : Application() {
@@ -11,6 +12,7 @@ class MedicationReminderApp : Application() {
         super.onCreate()
         setupTimber()
         setupHomeActivity()
+        setupPillDetailActivity()
     }
 
     private fun setupTimber() {
@@ -21,5 +23,9 @@ class MedicationReminderApp : Application() {
 
     private fun setupHomeActivity() {
         HomeActivity.mPillsRepository = LocalPillsPillsRepository()
+    }
+
+    private fun setupPillDetailActivity() {
+        PillDetailActivity.mPillsRepository = LocalPillsPillsRepository()
     }
 }
