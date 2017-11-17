4a48aae864dad0c685d3335ddd44d603ce0e6fe8
Alex <zhukovich.a@gmail.com>
2017-11-17 19:02:42 +0100

    [Refactoring] Extracted DAO to variable

--- app/src/androidTest/java/com/alexzh/medicationreminder/data/PillDaoTest.kt
+++ app/src/androidTest/java/com/alexzh/medicationreminder/data/PillDaoTest.kt
@@ -6,6 +6,7 @@ import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
 import com.alexzh.medicationreminder.TestData
 import com.alexzh.medicationreminder.data.local.MedicationReminderDatabase
+import com.alexzh.medicationreminder.data.local.PillDao
 import com.alexzh.medicationreminder.data.model.Pill
 import org.junit.Before
 import org.junit.Rule
@@ -19,7 +20,7 @@ class PillDaoTest {
         val EMPTY_PILL_LIST = listOf<Pill>()
     }
 
-    private lateinit var mDatabase: MedicationReminderDatabase
+    private lateinit var mPillDao: PillDao
 
     @Rule @JvmField
     val instantTaskExecutorRule = InstantTaskExecutorRule()
@@ -27,40 +28,41 @@ class PillDaoTest {
     @Before
     fun setUp() {
         val context = InstrumentationRegistry.getContext()
-        mDatabase = Room.inMemoryDatabaseBuilder(context, MedicationReminderDatabase::class.java)
+        val database = Room.inMemoryDatabaseBuilder(context, MedicationReminderDatabase::class.java)
                 .allowMainThreadQueries()
                 .build()
+        mPillDao = database.pillDao()
     }
 
     @Test
     fun shouldInsertPill() {
-        mDatabase.pillDao().insertPill(TestData.getFirstPill())
+        mPillDao.insertPill(TestData.getFirstPill())
 
-        mDatabase.pillDao().getPills()
+        mPillDao.getPills()
                            .test()
                            .assertValue(listOf(TestData.getFirstPill()))
     }
 
     @Test
     fun shouldUpdateExistingPill() {
-        mDatabase.pillDao().insertPill(TestData.getSecondPill())
+        mPillDao.insertPill(TestData.getSecondPill())
 
         val pill = TestData.getSecondPill().copy(
                 name = "updated second pill name",
                 description = "updated second pill description")
 
-        mDatabase.pillDao().updatePill(pill)
+        mPillDao.updatePill(pill)
 
-        mDatabase.pillDao().getPills()
+        mPillDao.getPills()
                 .test()
                 .assertValue(listOf(pill))
     }
 
     @Test
     fun shouldUpdateNotInsertNewPill() {
-        mDatabase.pillDao().updatePill(TestData.getFirstPill())
+        mPillDao.updatePill(TestData.getFirstPill())
 
-        mDatabase.pillDao().getPills()
+        mPillDao.getPills()
                 .test()
                 .assertValue(EMPTY_PILL_LIST)
     }
