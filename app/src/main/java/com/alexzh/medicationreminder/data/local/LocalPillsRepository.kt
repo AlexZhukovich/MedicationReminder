b75e923839ef70d82d83bd86290bc93c4c6707a0
Alex <zhukovich.a@gmail.com>
2017-11-18 13:11:14 +0100

    [Refactoring] Added comments to the LocalPillsRepository

--- app/src/main/java/com/alexzh/medicationreminder/data/local/LocalPillsRepository.kt
+++ app/src/main/java/com/alexzh/medicationreminder/data/local/LocalPillsRepository.kt
@@ -4,8 +4,22 @@ import com.alexzh.medicationreminder.data.PillsRepository
 import com.alexzh.medicationreminder.data.model.Pill
 import io.reactivex.Single
 
+/**
+ * Access Point for managing pills data.
+ */
 class LocalPillsRepository(private val pillDao: PillDao) : PillsRepository {
+    /**
+     * Get pills from the database.
+     *
+     * @return the users from the database.
+     */
     override fun getPills(): Single<List<Pill>> = pillDao.getPills()
 
+    /**
+     * Get pill by ID from the database.
+     *
+     * @param id the id of the pill.
+     * @return the pill from the database.
+     */
     override fun getPillById(id: Long): Single<Pill> = pillDao.getPillById(id)
 }
