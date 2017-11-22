6786d6848bb6dd7136b274e8dc1c345d3071110a
Alex <zhukovich.a@gmail.com>
2017-11-22 20:00:23 +0100

    [Refactoring] Updated fields for a Pill and Reminder entities

--- app/src/main/java/com/alexzh/medicationreminder/data/model/Pill.kt
+++ app/src/main/java/com/alexzh/medicationreminder/data/model/Pill.kt
@@ -1,10 +1,11 @@
 package com.alexzh.medicationreminder.data.model
 
+import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
 
 @Entity(tableName = "pills")
-data class Pill(@PrimaryKey(autoGenerate = true) val id: Long,
-                val name: String,
-                val description: String,
-                val dosage: String)
+data class Pill(@PrimaryKey(autoGenerate = true) @ColumnInfo(name = "pill_id") val id: Long,
+                @ColumnInfo(name = "pill_name") val name: String,
+                @ColumnInfo(name = "pill_description") val description: String,
+                @ColumnInfo(name = "pill_dosage") val dosage: String)
--- app/src/main/java/com/alexzh/medicationreminder/data/model/Reminder.kt
+++ app/src/main/java/com/alexzh/medicationreminder/data/model/Reminder.kt
@@ -7,6 +7,6 @@ import java.util.Date
 
 @Entity(tableName = "reminders")
 data class Reminder(@PrimaryKey(autoGenerate = true) @ColumnInfo(name = "reminder_id") val reminderId: Long,
-                    @ColumnInfo(name = "start_date") val startDate: Date,
-                    @ColumnInfo(name = "end_date") val endDate: Date,
+                    @ColumnInfo(name = "reminder start_date") val startDate: Date,
+                    @ColumnInfo(name = "reminder end_date") val endDate: Date,
                     @ColumnInfo(name = "reminder_time") val reminderTime: Date)
