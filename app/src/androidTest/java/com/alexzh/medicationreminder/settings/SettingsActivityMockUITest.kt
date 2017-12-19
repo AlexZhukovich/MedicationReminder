a000ac6a6db48dabf0e0601863934c389c0e7dd1
Alex <zhukovich.a@gmail.com>
2017-12-19 22:14:46 +0100

    Added the shouldCheckNavigationUpButton test case

--- app/src/androidTest/java/com/alexzh/medicationreminder/settings/SettingsActivityMockUITest.kt
+++ app/src/androidTest/java/com/alexzh/medicationreminder/settings/SettingsActivityMockUITest.kt
@@ -2,7 +2,9 @@ package com.alexzh.medicationreminder.settings
 
 import android.content.pm.PackageManager
 import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions
 import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers
 import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
 import android.support.test.espresso.matcher.ViewMatchers.withText
 import android.support.test.rule.ActivityTestRule
@@ -11,17 +13,24 @@ import com.alexzh.medicationreminder.data.AppInfoRepository
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.whenever
 import io.reactivex.subjects.SingleSubject
+import org.hamcrest.CoreMatchers.not
+import org.hamcrest.CoreMatchers.startsWith
 import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.ExpectedException
 
 class SettingsActivityMockUITest {
 
     companion object {
+        private val NAVIGATE_UP_DESCRIPTION = "Navigate up"
         val APP_VERSION = "0.0.1-dev"
     }
 
     private val mRepository = mock<AppInfoRepository>()
     private val mAppInfoSubject = SingleSubject.create<String>()
+
+    @Rule @JvmField
+    var thrown = ExpectedException.none()
 
     @Rule @JvmField
     val mActivityRule = object: ActivityTestRule<SettingsActivity>(SettingsActivity::class.java) {
@@ -58,5 +67,25 @@ class SettingsActivityMockUITest {
 
         onView(withText(R.string.pref_summary_app_version_unknown))
                 .check(matches(isDisplayed()))
+    }
+
+    /**
+     * Test wait for exception, because Activity should be finished.
+     */
+    @Test
+    fun shouldCheckNavigationUpButton() {
+        // Preparing expected exception.
+        thrown.expect(RuntimeException::class.java)
+        thrown.expectMessage(startsWith("No activities found."))
+
+        onView(withText(R.string.pref_title_category_about))
+                .check(matches(isDisplayed()))
+
+        onView(ViewMatchers.withContentDescription(NAVIGATE_UP_DESCRIPTION))
+                .check(matches(isDisplayed()))
+                .perform(ViewActions.click())
+
+        onView(withText(R.string.pref_title_category_about))
+                .check(matches(not(isDisplayed())))
     }
 }
