e9e304bfb24d11ababdc5d0b0f19328520690c1d
Alex <zhukovich.a@gmail.com>
2017-12-24 18:35:07 +0100

    Updated the buddybuild_postbuild.sh file

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-#echo "ConnectedCheck: "
-#./gradlew connectedCheck
+echo "ConnectedCheck: "
+./gradlew connectedCheck
 
 echo "Send report to CodeCov"
 bash <(curl -s https://codecov.io/bash) -t ${CODECOV_TOKEN}
